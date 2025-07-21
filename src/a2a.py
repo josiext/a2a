9291ae0f715393ccb98a8e678f5245c6862fca02
José Núñez <79015124+josiext@users.noreply.github.com>
2025-07-21 17:54:37 -0400

    feat: integrate HumanMessage and SystemMessage for improved response generation

--- src/a2a.py
+++ src/a2a.py
@@ -6,6 +6,7 @@ from IPython.display import Image, display
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
 from langchain.chat_models import init_chat_model
+from langchain_core.messages import HumanMessage, SystemMessage
 
 class State(TypedDict):
     # Messages have the type "list". The `add_messages` function
@@ -24,9 +25,16 @@ graph_builder.add_node("chatbot", chatbot)
 graph_builder.add_edge(START, "chatbot")
 graph_builder.add_edge("chatbot", END)
 graph = graph_builder.compile()
+
 display(Image(graph.get_graph().draw_mermaid_png()))
 
 def get_a2a_response(prompt: str, thread_id: str = None):
-    for event in graph.stream({"messages": [{"role": "user", "content": prompt}]}):
+    config = {"configurable": {"thread_id": thread_id}}
+    messages = [
+        SystemMessage(content="Eres un modelo de IA que responde preguntas de manera clara y concisa. Tu nombre es Jarvis."),
+        HumanMessage(content=prompt)
+    ]
+    for event in graph.stream({"messages": messages}, config):
         for value in event.values():
             print("Assistant:", value["messages"][-1].content)
+            return value["messages"][-1].content
