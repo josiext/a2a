fb0784672f46f34a8ff83f2e2ecba7f50c9384a7
José Núñez <79015124+josiext@users.noreply.github.com>
2025-07-22 10:10:51 -0400

    feat: integrate TavilySearch and enhance chatbot functionality with tool support

--- src/a2a.py
+++ src/a2a.py
@@ -1,32 +1,44 @@
 from typing import Annotated
 
-from typing_extensions import TypedDict
-from IPython.display import Image, display
-
-from langgraph.graph import StateGraph, START, END
-from langgraph.graph.message import add_messages
 from langchain.chat_models import init_chat_model
+from langchain_tavily import TavilySearch
+from langchain_core.messages import BaseMessage
+from typing_extensions import TypedDict
+from langchain.chat_models import init_chat_model
+
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph import StateGraph
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
 from langchain_core.messages import HumanMessage, SystemMessage
 
 class State(TypedDict):
-    # Messages have the type "list". The `add_messages` function
-    # in the annotation defines how this state key should be updated
-    # (in this case, it appends messages to the list, rather than overwriting them)
     messages: Annotated[list, add_messages]
-
-llm = init_chat_model("gemini-2.5-flash-lite-preview-06-17", model_provider="google_genai")
-
-def chatbot(state: State):
-    return {"messages": [llm.invoke(state["messages"])]}
 
 graph_builder = StateGraph(State)
 
-graph_builder.add_node("chatbot", chatbot)
-graph_builder.add_edge(START, "chatbot")
-graph_builder.add_edge("chatbot", END)
-graph = graph_builder.compile()
+tool = TavilySearch(max_results=2)
+tools = [tool]
 
-display(Image(graph.get_graph().draw_mermaid_png()))
+llm = init_chat_model("gemini-2.5-flash-lite-preview-06-17", model_provider="google_genai")
+llm_with_tools = llm.bind_tools(tools)
+
+def chatbot(state: State):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+graph_builder.add_node("chatbot", chatbot)
+
+tool_node = ToolNode(tools=[tool])
+graph_builder.add_node("tools", tool_node)
+
+graph_builder.add_conditional_edges(
+    "chatbot",
+    tools_condition,
+)
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.set_entry_point("chatbot")
+memory = InMemorySaver()
+graph = graph_builder.compile(checkpointer=memory)
 
 def get_a2a_response(prompt: str, thread_id: str = None):
     config = {"configurable": {"thread_id": thread_id}}
@@ -34,7 +46,6 @@ def get_a2a_response(prompt: str, thread_id: str = None):
         SystemMessage(content="Eres un modelo de IA que responde preguntas de manera clara y concisa. Tu nombre es Jarvis."),
         HumanMessage(content=prompt)
     ]
-    for event in graph.stream({"messages": messages}, config):
-        for value in event.values():
-            print("Assistant:", value["messages"][-1].content)
-            return value["messages"][-1].content
+
+    response = graph.invoke({"messages": messages}, config)
+    return response["messages"][-1].content
